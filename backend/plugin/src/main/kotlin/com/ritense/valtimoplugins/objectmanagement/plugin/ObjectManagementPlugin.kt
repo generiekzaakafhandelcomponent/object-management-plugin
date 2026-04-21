fe3067fef47fb28dc63de4b0690e86d0d7d35e51
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-04-21 14:53:23 +0200

    Merge getting-started and example-application docs, clean up README

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/objectmanagement/plugin/ObjectManagementPlugin.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/objectmanagement/plugin/ObjectManagementPlugin.kt
@@ -30,7 +30,7 @@ import com.ritense.valtimoplugins.objectmanagement.service.ObjectManagementCrudS
 import com.ritense.valueresolver.ValueResolverService
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
-import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.operaton.bpm.engine.delegate.DelegateExecution
 import java.net.URI
 import java.util.*
 
